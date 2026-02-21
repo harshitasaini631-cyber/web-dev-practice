b28acfdd7827701525afa14b76947e0549836191
Harshita saini <harshitasaini631@gmail.com>
2026-02-21 20:47:09 +0530

    Adding folder for DOM AND ITS STRUCTURE

---
+++ js/Dom-and-dom-structure/script.js
@@ -0,0 +1,6 @@
+const box = document.getElementById("box");
+
+console.log(box.parentElement);     // gives body
+console.log(box.children);          // gives h2 and p
+console.log(box.firstElementChild); // h2
+console.log(box.lastElementChild);  // p
