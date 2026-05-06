8f97169d60b02e1f0faf3446025e2e907027e8a4
Harshita saini <harshitasaini631@gmail.com>
2026-05-06 22:00:47 +0530

    callback function

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const box = document.getElementById("box");
-
-console.log(box.parentElement);     // gives body
-console.log(box.children);          // gives h2 and p
-console.log(box.firstElementChild); // h2
-console.log(box.lastElementChild);  // p
